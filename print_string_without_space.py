d5771c5ba223e3e6af2c7fc90580b2906221b839
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 13:01:53 +0800

    used r strip not working when printed

--- print_string_without_space.py
+++ print_string_without_space.py
@@ -1,5 +1,8 @@
 # Input name.
+user_name = str(input("Enter your full name: "))
 
 # Remove spaces behind inputted name.
+modified_name = user_name.rstrip()
 
 # Print modified name.
+print(modified_name)
