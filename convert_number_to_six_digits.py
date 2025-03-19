5bd9b490a157f94e8a9e9a8e2b25a8039bb60a6f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 14:01:21 +0800

    removed dot after comments

--- convert_number_to_six_digits.py
+++ convert_number_to_six_digits.py
@@ -1,10 +1,10 @@
-# Input number 1-1000.
+# Input number 1-1000
 user_num = int(input("Enter number 1-1000: "))
 
-# Add zeros at beginning of number to make it 6 digits. Print 6-digit number.
+# Add zeros at beginning of number to make it 6 digits. Print 6-digit number
 if 1 <= user_num <= 1000:
     print(str(user_num).zfill(6))
 
-# Remind user if inputted number is greater than 1000.
+# Remind user if inputted number is greater than 1000
 else:
     print("Number inputted should be 1-1000")
--- print_string_without_space.py
+++ print_string_without_space.py
@@ -1,5 +1,5 @@
-# Input name.
+# Input name
 user_name = str(input("Enter your full name: "))
 
-# Remove spaces behind inputted name. Print modified name.
+# Remove spaces behind inputted name. Print modified name
 print(user_name.lstrip())
