f5daa883abbd24dd97d16c97f1e073150d386054
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 14:59:08 +0800

    added input and print function consisting title function to convert letters to proper case

--- string_to_proper_casing.py
+++ string_to_proper_casing.py
@@ -1,4 +1,5 @@
 # Input full name without proper casing
+user_name = str(input("Enter your full name without proper casing: "))
 
 # Convert the inputted letters to proper casing then print proper cased letters
-
+print(user_name.title())
