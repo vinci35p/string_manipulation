fc578a32cd83c7da49a3fcb7ef3c2072489e44ef
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 13:33:48 +0800

    code too long, used z-fill instead, added else for number exceeding 1-1000 range

--- convert_number_to_six_digits.py
+++ convert_number_to_six_digits.py
@@ -1,18 +1,10 @@
 # Input number 1-1000.
 user_num = int(input("Enter number 1-1000: "))
 
-# Add zeros at beginning of number to make it 6 digits.
-num_count = len(str(user_num))
-if num_count == 1:
-    print(f"00000{user_num}")
-elif num_count == 2:
-    print(f"0000{user_num}")
-elif num_count == 3:
-    print(f"000{user_num}")
-elif num_count == 4:
-    if user_num <= 1000:
-        print(f"00{user_num}")
-    else:
-        print("Number inputted should be 1-1000")
+# Add zeros at beginning of number to make it 6 digits. Print 6-digit number.
+if 1 <= user_num <= 1000:
+    print(str(user_num).zfill(6))
 
-# Print 6-digit number.
+# Remind user if inputted number is greater than 1000.
+else:
+    print("Number inputted should be 1-1000")
