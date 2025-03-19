771f86c43404306d08d5050bb6ebd67f5191b89c
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 14:39:45 +0800

    used upper function to convert lower to upper case

--- lowercase_string_to_upper.py
+++ lowercase_string_to_upper.py
@@ -1,5 +1,8 @@
-# Input name
+# Input full name
+user_name = str(input("Enter your full name: "))
 
 # Convert lower case letters to upper case
+upper_case = user_name.upper()
 
 # Print converted letters
+print(upper_case)
