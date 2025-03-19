4d2187a0c30f254fc9b3ac0fa347a4c1d5aa8f73
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 13:05:29 +0800

    made the code concise and merged print and strip function

--- print_string_without_space.py
+++ print_string_without_space.py
@@ -1,8 +1,5 @@
 # Input name.
 user_name = str(input("Enter your full name: "))
 
-# Remove spaces behind inputted name.
-modified_name = user_name.lstrip()
-
-# Print modified name.
-print(modified_name)
+# Remove spaces behind inputted name. Print modified name.
+print(user_name.lstrip())
