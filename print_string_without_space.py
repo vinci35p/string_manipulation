1bcc1f12d7f406e0fae53aec01bd47f8d243438d
Vincent35p <vinsoyt4321@gmail.com>
2025-03-19 13:03:26 +0800

    used l strip since leading spaces now working

--- print_string_without_space.py
+++ print_string_without_space.py
@@ -2,7 +2,7 @@
 user_name = str(input("Enter your full name: "))
 
 # Remove spaces behind inputted name.
-modified_name = user_name.rstrip()
+modified_name = user_name.lstrip()
 
 # Print modified name.
 print(modified_name)
