e8a55ec57c7db16b55f936d2642abb56f781ed8f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-20 22:55:25 +0800

    added len function to count characters, then f string print func to indicate number of characters in inputted name

--- count_characters.py
+++ count_characters.py
@@ -1,5 +1,8 @@
 # Input full name
+user_name = str(input("Enter your full name: "))
 
 # Count characters in inputted string
+characters_count = len(user_name)
 
 # Print counted characters
+print(f"The number of characters on your full name is {characters_count}.")
