d30674255878f0eb66eb81fbd74a87c7094a48d8
Vincent35p <vinsoyt4321@gmail.com>
2025-03-20 22:40:57 +0800

    added split function and len to count split words, then print f string func to indicate counted words

--- count_words_in_sentence.py
+++ count_words_in_sentence.py
@@ -1,5 +1,8 @@
 # Input a sentence
+user_sentence = str(input("Enter a sentence: "))
 
 # Count every word in sentence inputted
+word_count = len(user_sentence.split())
 
 # Print count value
+print(f"The count of words in the sentence is {word_count}.")
