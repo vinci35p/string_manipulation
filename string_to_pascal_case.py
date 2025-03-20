2a51a69094006585c1a16f7bdb821390569f1be5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-20 23:01:59 +0800

    added title function to make the name in proper case then replace function to erase spaces, then print the formatted value

--- string_to_pascal_case.py
+++ string_to_pascal_case.py
@@ -1,5 +1,8 @@
-# Input full name
+# Input full name in incorrect casing
+user_name = str(input("Enter your full name in incorrect casing: "))
 
 # Turn inputted name into pascal case
+pascal_name = user_name.title().replace(" ", "")
 
 # Print pascal cased name
+print(pascal_name)
