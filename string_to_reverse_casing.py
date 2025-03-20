27f72af9e5a9785b8f8ee32e668024743c4d7b3f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-20 22:35:03 +0800

    added swap case func to reverse string, then print func for new added variable reversed_name

--- string_to_reverse_casing.py
+++ string_to_reverse_casing.py
@@ -1,5 +1,8 @@
 # Input full name in incorrect casing
+user_name = str(input("Enter your full name in incorrect casing: "))
 
 # Reverse the casing inputted
+reversed_name = user_name.swapcase()
 
 # Print the reversed case name
+print(reversed_name)
