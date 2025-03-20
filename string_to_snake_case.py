5bf183056793b20ab1d8f6154af9fd71c151cb82
Vincent35p <vinsoyt4321@gmail.com>
2025-03-20 23:15:19 +0800

    added lower func to convert upper case letters to lower, then replace func for spaces to underscores

--- string_to_snake_case.py
+++ string_to_snake_case.py
@@ -1,5 +1,8 @@
 # Input full name in incorrect casing
+user_name = str(input("Enter your full name in incorrect casing: "))
 
 # Turn inputted name into snake case
+snake_case_name = user_name.lower().replace(" ", "_")
 
 # Print snake cased name
+print(snake_case_name)
